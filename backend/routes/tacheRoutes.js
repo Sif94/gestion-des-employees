c76236228717e1e6ea6d577969ee39fdbcecd649
Sifeddine <sifeddinebaouz@gmail.com>
2023-12-16 21:16:59 +0100

    L'ajout des taches et heures supp

---
+++ backend/routes/tacheRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createTache, deleteTacheById, getAllTaches, getTacheById, updateTacheById } from "../controllers/tacheController.js";
+import {isAuth, isAuthorized} from "../middlewares/protectedRoutesMiddleware.js"
+
+
+const router = express.Router();
+
+router.get('/', [isAuth, isAuthorized("Admin", "RH")], getAllTaches)
+router.post('/', [isAuth, isAuthorized("Admin", "RH")], createTache)
+router.get('/:id', [isAuth, isAuthorized("Admin", "RH")], getTacheById)
+router.put('/update/:id', [isAuth, isAuthorized("Admin", "RH")], updateTacheById)
+router.delete('/delete/:id', [isAuth, isAuthorized("Admin", "RH")], deleteTacheById)
+
+
+export default router
