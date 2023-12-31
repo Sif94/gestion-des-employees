8e49ccea3f6777cc2d69798380b2c8377abf0a41
Sifeddine <sifeddinebaouz@gmail.com>
2023-12-31 22:00:44 +0100

    .

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,12 +8,9 @@ const Home = () => {
   <div className="text-center text-white">
     <h1 className="text-4xl font-bold mb-4">Gestion des Employés</h1>
     <p className="text-lg mb-8">Optimisez la gestion de votre équipe avec notre application.</p>
-    <div className="flex justify-center">
-      <Button onClick={() => navigate('/login')} className="bg-blue-500 text-white px-6 py-3 rounded-full text-lg mr-4">
+    <div className="flex justify-center items-center">
+      <Button onClick={() => navigate('/dashboard')} className="bg-blue-500 text-white px-6 py-3 rounded-full text-lg mr-4">
         Commencer
-      </Button>
-      <Button onClick={() => navigate('/help')} className="bg-gray-500 text-white px-6 py-3 rounded-full text-lg">
-        En savoir plus
       </Button>
     </div>
   </div>
